79bf5d125c016a78f26227485aeadee20149e029
pierry01 <pierrybm@gmail.com>
2019-08-17 01:38:04 +0000

    moving the task model to the correct place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -8,21 +8,4 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'TaskManager'
-  
-  task1: Task = {
-    id: 21,
-    title: 'Título XXX'
-  }
-  
-  task2: Task = new Task(22, 'Título YYY')
-}
-
-export class Task {
-  public id: number
-  public title: string
-  
-  constructor(id: number, title: string) {
-    this.id = id
-    this.title = title
-  }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core'
 
-const TASKS: Array<any> = [
+import { Task } from './shared/task.model'
+
+const TASKS: Array<Task> = [
   { id: 1, title: 'Fazer tarefa 1' },
   { id: 2, title: 'Fazer tarefa 2' },
   { id: 3, title: 'Fazer tarefa 3' },
-  { id: 4, title: 'Fazer tarefa 4' },
-  { id: 5, title: 'Fazer tarefa 5' },
-  { id: 6, title: 'Fazer tarefa 6' },
-  { id: 7, title: 'Fazer tarefa 7' }
+  { id: 4, title: 'Fazer tarefa 4' }
 ]
 
 @Component({
