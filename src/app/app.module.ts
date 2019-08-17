4d532aa0f423ec201b2d72b5b9a10ddfa182fc5f
Ubuntu <ubuntu@ip-172-31-82-129.ec2.internal>
2019-08-17 01:02:15 +0000

    adding navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { NavbarComponent } from './navbar/navbar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
