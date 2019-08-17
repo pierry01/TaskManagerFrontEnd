55c9206d44c0dc5a3b343377387e3fd5a52470a6
Ubuntu <ubuntu@ip-172-31-82-129.ec2.internal>
2019-08-17 01:27:06 +0000

    adding tasks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { NavbarComponent } from './navbar/navbar.component'
+import { TasksComponent } from './tasks/tasks.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
