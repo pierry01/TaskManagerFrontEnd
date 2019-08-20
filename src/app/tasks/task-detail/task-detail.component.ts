a0d2a2688f27f44433785d6fdefdf5ca6e8adb27
pierry01 <pierrybm@gmail.com>
2019-08-20 17:05:46 +0000

    removing unnecessary code

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, Params } from '@angular/router'
 
 import { switchMap } from 'rxjs/operators'
@@ -12,7 +12,7 @@ import { TaskService } from '../shared/task.service'
 })
 
 export class TaskDetailComponent implements OnInit{
-  @Input() task: Task
+  task: Task
   
   constructor(
     private taskService: TaskService,
